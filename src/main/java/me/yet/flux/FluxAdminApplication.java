f3ac53891e4ef1808709eeaba7926c4c2526d09b
dspos <1766078228@qq.com>
2022-08-24 02:03:50 +0800

    init project

---
+++ src/main/java/me/yet/flux/FluxAdminApplication.java
@@ -0,0 +1,13 @@
+package me.yet.flux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/yet/flux/FluxAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yet.flux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
